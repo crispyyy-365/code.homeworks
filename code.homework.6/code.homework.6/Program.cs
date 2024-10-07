98730fa69b53427d8f84cfd0f80678e08c6d02e3
Huseyn Abbasov <huseynaa-bp217@code.edu.az>
2024-10-07 13:01:23 +0400

    ayin 2-den bir tapsiriq daha

---
+++ code.homework.6/code.homework.6/Program.cs
@@ -0,0 +1,26 @@
+﻿//Ədədlərdən ibarət arrayın ən kiçik elementini tapın
+
+int[] num = { 9, 16, 4, 23, 14 };
+
+int count = 0;
+
+Console.WriteLine("arrayin en kicik elementi : ");
+
+for (int i = 0; i < num.Length; i++)
+{
+    for (int j = 0; j < num.Length; j++)
+    {
+        if (num[i] > num[j])
+        {
+            count++;
+        }
+    }
+    if (count > 0)
+    {
+        count = 0;
+    }
+    else
+    {
+        Console.WriteLine(num[i]);
+    }
+}
