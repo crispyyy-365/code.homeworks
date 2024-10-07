0e044eb3abdb72777a0aecf2b645593b78232ca0
Huseyn Abbasov <huseynaa-bp217@code.edu.az>
2024-10-07 12:58:30 +0400

    ayin 2 nin tapsiriqlari

---
+++ homework.4/homework.4/Program.cs
@@ -0,0 +1,12 @@
+﻿// ededlerden ibaret arrayin ededlerinin cemini tap
+
+using System.Net.Http.Headers;
+
+int[] num = { 56, 72, 88, 49 };
+int sum = 0;
+
+for (int i = 0; i < num.Length; i++)
+{
+    sum = sum + num[i];
+}
+Console.WriteLine(sum);
---
+++ homework5/homework5/Program.cs
@@ -0,0 +1,15 @@
+﻿// Sozlerden ibaret arrayde uzunlugu 4-den boyuk olan sozleri ekrana çap edin
+
+string[] words = { "apple", "pea", "computer", "car", "watch", "book", "guitar" };
+
+for (int i = 0; i < words.Length; i++)
+{
+    for (int j = 0; j < words[i].Length; j++)
+    {
+        if (words[i].Length > 4)
+        {
+            Console.WriteLine(words[i]);
+            break;
+        }
+    }
+}
