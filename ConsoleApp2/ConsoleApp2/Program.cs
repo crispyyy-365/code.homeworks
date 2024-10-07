dbb9f2e15333c139d6ab98b3ca09552667c456e2
Huseyn Abbasov <huseynaa-bp217@code.edu.az>
2024-10-07 14:59:33 +0400

    tasks

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,55 @@
+﻿string option;
+
+string[] arr = { "eli", "taceddin", "ayxan" };
+
+Console.WriteLine("1.Add contacts");
+
+Console.WriteLine("2.Show contacts");
+
+Console.WriteLine("3.Show maintained contact");
+
+do
+{
+    option = Console.ReadLine();
+
+    switch (option)
+    {
+        case "1":
+
+            Console.WriteLine("value : ");
+
+            string contact = Console.ReadLine();
+
+            Array.Resize(ref arr, arr.Length + 1);
+
+            arr[arr.Length - 1] = contact;
+
+            Console.WriteLine("contact added");
+
+            break;
+
+        case "2":
+            Console.WriteLine("these are your contacts : ");
+
+            for (int i = 0; i<arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            };
+            break;
+
+        case "3":
+
+            string name = Console.ReadLine();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (name == arr[i])
+                {
+                    Console.WriteLine(arr[i]);
+                }
+            };
+            break;
+    }
+
+}
+while (option != "0");
